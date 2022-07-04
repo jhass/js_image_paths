6da71e9754df2211f7cba597762b3b24a0ca6764
Benjamin Neff <benjamin@coding4coffee.ch>
2022-07-04 04:10:36 +0200

    Breaking change: only include images from app/assets/images
    Only checking if the path includes 'images' isn't safe, because if there
    is 'images' anywhere else in the path suddenly everything is an "image".
    Also, it tries to include itself in itself too, because it's an "image"
    too.
    
    But this also means that now images from other dependencies aren't
    included in the list anymore. But I didn't really find another way of
    safely detecting only images.

--- lib/js_image_paths/generator.rb
+++ lib/js_image_paths/generator.rb
@@ -8,11 +8,15 @@ module JsImagePaths
 
     def self.image_hash
       context.environment.each_file.each_with_object({}) do |path, images|
-        next unless path.include?('images')
+        next unless path.start_with?(app_images_path)
 
         asset = context.environment.find_asset(path)
         images[asset.logical_path] = context.asset_path(asset.logical_path)
       end
+    end
+
+    def self.app_images_path
+      @app_images_path ||= Rails.root.join('app/assets/images').to_s
     end
   end
 end
